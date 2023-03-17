b78a17c249b4cf255bd14372636bdfaf456cf3f3
Tiago França <tiago@tiagofranca.com>
2023-03-17 11:30:54 -0300

    adjust of routes
    On branch 1.x/dev
    Changes to be committed:
    	modified:   routes/surveys.php

--- routes/surveys.php
+++ routes/surveys.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Api\SurveyAnswerController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('surveys')->name('surveys.')->group(function () {
-    Route::post('/surveys/result_list', [SurveyAnswerController::class, 'surveyResultList'])->name('result_list');
-    Route::post('/surveys/answers', [SurveyAnswerController::class, 'newAnswer'])->name('new_answer');
+    Route::post('/result_list', [SurveyAnswerController::class, 'surveyResultList'])->name('result_list');
+    Route::post('/answers', [SurveyAnswerController::class, 'newAnswer'])->name('new_answer');
 });
